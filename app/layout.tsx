43cd1c6c6e88d6f90f419930f672fec363b1d2b6
RobertoBarrosoLuque <34043109+RobertoBarrosoLuque@users.noreply.github.com>
2025-07-24 21:32:48 -0700

    Add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: 'Chat - FireworksAI',
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
